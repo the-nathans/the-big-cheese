4b4b8532d06420ad95afb4f16553088c25620e97
Aaron Freytag <aaroncfreytag@gmail.com>
2017-01-28 14:46:19 -0800

    Changed website to use database

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,6 +19,23 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
+        $pix = $this->images->newest();
+        
+        foreach ($pix as $picture) {
+            $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+        }
+        
+        $this->load->library('table');
+        $params = array(
+            'table_open' => '<table class="gallery">',
+            'cell_start' => '<td class="oneimage">',
+            'cell_alt_start' => '<td class="oneimage">'
+        );
+        $this->table->set_template($params);
+        
+        $rows = $this->table->make_columns($cells, 3);
+        $this->data['thetable'] = $this->table->generate($rows);
+        
 		$this->data['pagebody'] = 'welcome';
 		$this->render(); 
 	}
